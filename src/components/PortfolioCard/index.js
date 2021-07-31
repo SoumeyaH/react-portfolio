9638427487207ce09e5564a6713a122bea7d155f
Soumeya <soumeyahassan47@gmail.com>
2021-07-31 17:28:23 +0100

    styled portfolio card

---
+++ src/components/PortfolioCard/index.js
@@ -0,0 +1,74 @@
+import Card from "react-bootstrap/Card";
+
+import "./PortfolioCard.css";
+
+const PortfolioCard = () => {
+  return (
+    <Card className="text-center my-4 mx-auto shadow-lg portfolio-card">
+      <Card.Header className="card-title">Event Jam</Card.Header>
+      <Card.Img variant="top" src="https://via.placeholder.com/150" />
+      <Card.Body>
+        <Card.Text>
+          Event Jam is a dynamic online application that offer users the ability
+          to search for events in any city in the UK and get relevant, reliable
+          and up to date Covid-19 information for that area.
+        </Card.Text>
+        <p class="m-2 fst-italic text-muted">
+          HTML/CSS, Bulma, JS (ES6), jQuery, Chart.js, Third-Party APIs, Git
+        </p>
+      </Card.Body>
+      <div class="d-flex flex-wrap justify-content-center">
+        <div>
+          <a href="https://soumeyah.github.io/event-jam/" target="_blank">
+            <button type="button" class="btn btn-light mx-3 my-1">
+              Deployed
+            </button>
+          </a>
+        </div>
+        <div>
+          <a href="https://github.com/SoumeyaH/event-jam" target="_blank">
+            <button type="button" class="btn btn-light mx-3 my-1">
+              Github
+            </button>
+          </a>
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default PortfolioCard;
+
+<div class="card text-center my-4 mx-auto shadow-lg portfolio-card">
+  <div class="card-header">Event Jam</div>
+  <div class="card-body">
+    <video controls muted class="card-img-top project-img">
+      <source src="./assets/videos/event-jam.mp4" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+    <p class="m-2">
+      Event Jam is a dynamic online application that offer users the ability to
+      search for events in any city in the UK and get relevant, reliable and up
+      to date Covid-19 information for that area.
+    </p>
+    <p class="m-2 fst-italic text-muted">
+      HTML/CSS, Bulma, JS (ES6), jQuery, Chart.js, Third-Party APIs, Git
+    </p>
+    <div class="d-flex flex-wrap justify-content-center">
+      <div>
+        <a href="https://soumeyah.github.io/event-jam/" target="_blank">
+          <button type="button" class="btn btn-light mx-3 my-1">
+            Deployed
+          </button>
+        </a>
+      </div>
+      <div>
+        <a href="https://github.com/SoumeyaH/event-jam" target="_blank">
+          <button type="button" class="btn btn-light mx-3 my-1">
+            Github
+          </button>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>;
--- src/pages/PortfolioPage/index.js
+++ src/pages/PortfolioPage/index.js
@@ -1,9 +1,11 @@
+import PortfolioCard from "../../components/PortfolioCard";
 import Title from "../../components/Title";
 
 const PortfolioPage = () => {
   return (
     <div className="portfolio-page-container">
       <Title text="Portfolio" />
+      <PortfolioCard />
     </div>
   );
 };
