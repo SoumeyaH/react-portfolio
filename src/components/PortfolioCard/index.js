3989edc97bba37d554292c3060ea09b06b8eddc6
Soumeya <soumeyahassan47@gmail.com>
2021-07-31 17:50:57 +0100

    installed fromspree with boilerplate

--- src/components/PortfolioCard/index.js
+++ src/components/PortfolioCard/index.js
@@ -38,37 +38,3 @@ const PortfolioCard = () => {
 };
 
 export default PortfolioCard;
-
-<div class="card text-center my-4 mx-auto shadow-lg portfolio-card">
-  <div class="card-header">Event Jam</div>
-  <div class="card-body">
-    <video controls muted class="card-img-top project-img">
-      <source src="./assets/videos/event-jam.mp4" type="video/mp4" />
-      Your browser does not support the video tag.
-    </video>
-    <p class="m-2">
-      Event Jam is a dynamic online application that offer users the ability to
-      search for events in any city in the UK and get relevant, reliable and up
-      to date Covid-19 information for that area.
-    </p>
-    <p class="m-2 fst-italic text-muted">
-      HTML/CSS, Bulma, JS (ES6), jQuery, Chart.js, Third-Party APIs, Git
-    </p>
-    <div class="d-flex flex-wrap justify-content-center">
-      <div>
-        <a href="https://soumeyah.github.io/event-jam/" target="_blank">
-          <button type="button" class="btn btn-light mx-3 my-1">
-            Deployed
-          </button>
-        </a>
-      </div>
-      <div>
-        <a href="https://github.com/SoumeyaH/event-jam" target="_blank">
-          <button type="button" class="btn btn-light mx-3 my-1">
-            Github
-          </button>
-        </a>
-      </div>
-    </div>
-  </div>
-</div>;
--- src/pages/ContactMePage/index.js
+++ src/pages/ContactMePage/index.js
@@ -1,9 +1,30 @@
+import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
+
 import Title from "../../components/Title";
 
 const ContactMePage = () => {
+  const [state, handleSubmit] = useForm("mjvjawnb");
+  if (state.succeeded) {
+    return <p>Thanks for joining!</p>;
+  }
   return (
     <div className="contact-me-page-container">
       <Title text="Contact Me" />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email Address</label>
+        <input id="email" type="email" name="email" />
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
+        <textarea id="message" name="message" />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
+        <button type="submit" disabled={state.submitting}>
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
