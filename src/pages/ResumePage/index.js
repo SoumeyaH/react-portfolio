2723183a77b9c36b853c4a7ee8c130f4df12da1e
Soumeya <soumeyahassan47@gmail.com>
2021-08-01 17:32:42 +0100

    fix resume link

--- src/pages/ResumePage/index.js
+++ src/pages/ResumePage/index.js
@@ -1,5 +1,9 @@
 const ResumePage = () => {
-  return <div>resume</div>;
+  window.open(
+    "https://soumeyah.github.io/portfolio-2/SoumeyaHassanResume.pdf",
+    "_blank"
+  );
+  return null;
 };
 
 export default ResumePage;
