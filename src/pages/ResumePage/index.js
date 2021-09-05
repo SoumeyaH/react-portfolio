26c534d0f2a64835f08ca3ff54a6705053a8d4e6
Soumeya <soumeyahassan47@gmail.com>
2021-09-05 20:23:47 +0100

    Updates

--- src/pages/ResumePage/index.js
+++
@@ -1,9 +0,0 @@
-const ResumePage = () => {
-  window.open(
-    "https://soumeyah.github.io/portfolio-2/SoumeyaHassanResume.pdf",
-    "_blank"
-  );
-  return null;
-};
-
-export default ResumePage;
